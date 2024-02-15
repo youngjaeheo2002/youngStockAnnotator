d8373f840bccb5a526592821a439d48c54b97c70
youngjaeheo2002 <youngjaeheo2002@gmaill.com>
2024-02-15 16:41:20 -0500

    skeleton

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # young_time_series_annotator
+
+
+i'm making a web app that allows users to upload timeseries data files and add annotations to them
---
+++ server/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((error) => console.error('Could not connect to MongoDB:', error));
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
